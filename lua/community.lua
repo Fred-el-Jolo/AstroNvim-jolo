beb7c7aa658805fd94cb955b4b7d166909c4ff95
Fred-el-jolo <guillaume.frederic@gmail.com>
2026-09-10 12:00:30 +0200

    Setup vue config

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +6,8 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.vue" }, -- vtsls + volar hybrid mode, covers src/vue (TS+Vue)
+  { import = "astrocommunity.pack.eslint" }, -- eslint LSP, covers src/js (root) and src/vue
+  { import = "astrocommunity.pack.html-css" }, -- cssls + less/scss treesitter, covers src/css (LESS)
   -- import/override with your plugins folder
 }
